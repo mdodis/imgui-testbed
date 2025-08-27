c2bdca906b7db85a8f02533c203208a0b10d6a24
Michael Dodis <michaeldodisgr@gmail.com>
2025-08-27 08:10:13 +0200

    add build for windows

---
+++ imgui_library.cpp
@@ -0,0 +1,10 @@
+#include "imgui/imgui.cpp"
+#include "imgui/imgui_demo.cpp"
+#include "imgui/imgui_draw.cpp"
+#include "imgui/imgui_tables.cpp"
+#include "imgui/imgui_widgets.cpp"
+
+#ifdef _WIN32
+#include "imgui/backends/imgui_impl_opengl3.cpp"
+#include "imgui/backends/imgui_impl_win32.cpp"
+#endif // _WIN32
---
+++ scripts/windows/build.bat
@@ -0,0 +1,39 @@
+@echo off
+
+set SCRIPT_DIR=%~dp0
+
+set COMPILE_ALL=0
+
+:parse_args
+if "%~1"=="" goto after_parse
+
+if "%~1"=="-a" (
+    set COMPILE_ALL=1
+) else (
+    echo Unknown option: %~1
+)
+
+pushd %SCRIPT_DIR%\..\..\
+
+if not exist build_windows mkdir build_windows
+
+pushd build_windows
+
+if %COMPILE_ALL%==1 (
+    cl ^
+        /c /nologo /EHsc /Zi /Od /I ..\imgui\ ^
+        ..\imgui_library.cpp ^
+        /Fo:IMGUI.obj
+
+    lib /nologo IMGUI.obj 
+)
+
+
+cl ^
+    /nologo /EHsc /Zi /Od /I ..\imgui\ ^
+     ..\run_windows.cpp ^
+     /Fe:run_windows.exe /link .\IMGUI.lib opengl32.lib user32.lib kernel32.lib
+
+popd
+
+popd
